72e190fd3ea22789b85159de215545707f8e35f0
nasifahmedoney <nasif.ahmed@live.com>
2022-08-21 23:10:30 +0600

    middleware with app.use

--- main/express/final/5-middleware.js
+++ main/express/final/5-middleware.js
@@ -5,11 +5,17 @@ const logger =  require('./logger.js');
 // middleware functionality
 // request => middleware => response
 
-app.get('/',logger,(req,res)=>{
+
+//using app.use(logger), executes sequentially, i.e. using
+//app.use(logger) after '/' will onlly work on urls after '/'
+//using app.use('/api',logger) will only work on urls with '/api' and '/api/anything-comes-after-api'
+app.use(logger)
+
+app.get('/',(req,res)=>{
     res.send('Home page')
 })
 
-app.get('/about',logger,(req,res)=>{
+app.get('/about',(req,res)=>{
     res.send('About page')
 })
 app.listen(5000,()=>{
