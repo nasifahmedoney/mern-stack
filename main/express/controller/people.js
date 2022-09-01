c22aff4ea6c83f602e64d5d4e8bd765704a7db66
nasifahmedoney <nasif.ahmed@live.com>
2022-09-01 16:30:06 +0600

    controller

---
+++ main/express/controller/people.js
@@ -0,0 +1,49 @@
+const {people} = require('../data');
+
+const getPeople = (req,res)=>{
+    res.status(200).json({success:true, data: people})
+}
+
+const postPeople = (req,res)=>{
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success: false, msg:'name value empty'})
+    }
+    res.status(201).json({success: true, person: [...people,name]})
+}
+
+const putPeople = (req,res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const person = people.find((person)=>person.id===Number(id))
+
+    if(!person){
+       return res.status(404).json({success:false,msg:`${id} not found`})
+    } 
+    const newPeople = people.map((person)=>{
+        if(Number(id)===person.id){
+            person.name = name;
+        }
+        return person;
+    })
+    res.status(200).json({success:true, data:newPeople});
+}
+
+const deletePeople = (req,res)=>{
+    const person = people.find((person)=>person.id===Number(req.params.id))
+
+    if(!person){
+       return res.status(404).json({success:false,msg:`${req.params.id} not found`})
+    }
+
+    const newPeople = people.filter((user)=>user.id !== Number(req.params.id))
+
+    return res.status(200).json({success:true,data:newPeople})
+}
+
+module.exports = {
+    getPeople,
+    postPeople,
+    putPeople,
+    deletePeople
+}
--- main/express/routes/people.js
+++ main/express/routes/people.js
@@ -1,55 +1,18 @@
 const express = require('express')
 const router = express.Router();
-const {people} = require('../data');
 
-router.get('/',(req,res)=>{
-    res.status(200).json({success:true, data: people})
-})
+const {
+    getPeople,
+    postPeople,
+    putPeople,
+    deletePeople
+} = require('../controller/people')
 
-router.post('/',(req,res)=>{
-    const {name} = req.body;
-    if(!name){
-        return res.status(400).json({success: false, msg:'name value empty'})
-    }
-    res.status(201).json({success: true, person: [...people,name]})
-})
+router.get('/',getPeople)
+router.post('/',postPeople)
+router.put('/:id',putPeople)
+router.delete('/:id',deletePeople) 
 
-//put requests, single entry update
-router.put('/:id',(req,res)=>{
-    const {id} = req.params;
-    const {name} = req.body;//parse form data
-    /* name sent as json
-    {
-        "name": "user"
-    }
-     */
 
-    const person = people.find((person)=>person.id===Number(id))
-
-    if(!person){
-       return res.status(404).json({success:false,msg:`${id} not found`})
-    }
-    //returns the modified array of people from data.js, 
-    const newPeople = people.map((person)=>{
-        if(Number(id)===person.id){
-            person.name = name;
-        }
-        return person;
-    })
-    res.status(200).json({success:true, data:newPeople});
-})
-
-//delete method
-router.delete('/:id',(req,res)=>{
-    const person = people.find((person)=>person.id===Number(req.params.id))
-
-    if(!person){
-       return res.status(404).json({success:false,msg:`${req.params.id} not found`})
-    }
-
-    const newPeople = people.filter((user)=>user.id !== Number(req.params.id))
-
-    return res.status(200).json({success:true,data:newPeople})
-}) 
 
 module.exports = router;
