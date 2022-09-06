9346b1ba725e254b0d680e12f0a8cd0ee4ac8b6d
nasifahmedoney <nasif.ahmed@live.com>
2022-09-06 14:35:12 +0600

    using dotenv for connection string

--- main/task-manager/app.js
+++ main/task-manager/app.js
@@ -1,8 +1,8 @@
-require('./db/connection');
-
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connection');
 const tasks = require('./routes/tasks');
+require('dotenv').config();
 
 app.use(express.json())
 app.use('/api/v1/tasks',tasks);
@@ -10,4 +10,13 @@ app.use('/api/v1/tasks',tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening to port ${port}...`));
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`server is listening to port ${port}...`));
+    }catch(error){
+        console.log(error)
+    }
+}
+
+start()
--- main/task-manager/db/connection.js
+++ main/task-manager/db/connection.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose')
 
+const connectDB = (url) =>{
+    mongoose.connect(url)
+}
 
-
-const connectionString = 'mongodb+srv://Nasif:abcd1234@node-express-tutorial.nrzfwtf.mongodb.net/TaskManager?retryWrites=true&w=majority'
-
-mongoose
-.connect(connectionString)
-.then(()=>{
-    console.log('connected to database...')
-})
-.catch((err)=>{
-    console.log(err);
-})
+module.exports = connectDB;
