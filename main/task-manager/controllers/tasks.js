e169731ad4ae86be910134d882de8c1787bb38ff
nasifahmedoney <nasif.ahmed@live.com>
2022-09-07 13:54:10 +0600

    make post into database
    send json data from postman

--- main/task-manager/controllers/tasks.js
+++ main/task-manager/controllers/tasks.js
@@ -1,8 +1,11 @@
+const task = require('../models/DbSchema')
+
 const getAllItems = (req,res)=>{
     res.send('all items');
 }
-const createTask = (req,res)=>{
-    res.json(req.body);
+const createTask = async (req,res)=>{
+    const post = await task.create(req.body)//send json data from postman 
+    res.status(201).json(post);
 }
 const getTask = (req,res)=>{
     res.json({id: req.params.id});
